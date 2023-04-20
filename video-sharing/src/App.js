2fa4c0c959a0ee774db691da301f733d5d7af33d
Ankit <ankitkumarjune18@gmail.com>
2023-04-20 22:34:38 +0530

    added

--- video-sharing/src/App.js
+++ video-sharing/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Upload from "./components/upload-search-stream/Upload.jsx";
 
 import Router from "./routers/routes";
 
 function App() {
   return <>
     <Router/>
+    <Upload/>
     
     
     
--- video-sharing/src/routers/routes.js
+++ video-sharing/src/routers/routes.js
@@ -1,7 +1,8 @@
 import React from "react";
 import LandingPage from "../components/Home-Myvidoes/Landing";
-import Streaming from "../components/Upload-Search-Stream/Streaming";
-import Upload from "../components/Upload-Search-Stream/Upload";
+// import Upload from "../components/Upload-Search-Stream/Upload";
+// import Streaming from "../components/Upload-Search-Stream/Streaming";
+
 
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 function Router(){
@@ -9,8 +10,9 @@ function Router(){
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage/>} />
-                <Route path="/streaming" element={<Streaming/>}/>
-                <Route path="/Upload" element={<Upload/>}/>
+                {/* <Route path="/upload" element={<Upload/>}/>
+                <Route path="/streaming" element={<Streaming/>}/> */}
+                
                
             </Routes>
         </BrowserRouter>
