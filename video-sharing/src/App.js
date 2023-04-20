5c4bf69f4b4881fe1aad426a143899f48c877dd3
Ankit <ankitkumarjune18@gmail.com>
2023-04-20 18:36:29 +0530

    added

--- video-sharing/src/App.js
+++ video-sharing/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import UploadFile from "./components/Upload-Search-Stream/Upload.jsx";
+import Streaming from "./components/Upload-Search-Stream/Streaming.jsx"
 
 function App() {
   return <>
   <UploadFile/>
+  <Streaming/>
   
   </>
 }
---
+++ video-sharing/src/components/Upload-Search-Stream/Streaming.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Streaming.css";
+
+function StramingVideo(){
+    return(
+        <div className="main">
+            <div className="close-icon">
+                <img src="https://png.pngtree.com/png-vector/20190425/ourmid/pngtree-vector-cross-icon-png-image_992292.jpg"/>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default StramingVideo
