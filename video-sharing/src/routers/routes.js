6a24dd3799c5cdb0fdfb1ebda54e29ea4cf28013
Ankit <ankitkumarjune18@gmail.com>
2023-04-21 13:12:58 +0530

    commit

--- video-sharing/src/routers/routes.js
+++ video-sharing/src/routers/routes.js
@@ -1,10 +1,11 @@
 import React from "react";
 import LandingPage from "../components/Home-Myvidoes/Landing";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import Upload from "../components/Upload-Search-Stream/Upload";
 // import Streaming from "../components/Upload-Search-Stream/Streaming";
 
 
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+
 function Router(){
     return <>
         <BrowserRouter>
