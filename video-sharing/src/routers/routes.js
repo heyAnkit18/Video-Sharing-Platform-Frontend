876494055851a46685f3292c068953ead9897c4c
Amaanmohammad2000 <amaanmohammed2000@gmail.com>
2023-04-23 14:59:57 +0530

    Home page and myvidoes page

--- video-sharing/src/routers/routes.js
+++ video-sharing/src/routers/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import LandingPage from "../components/Home-Myvidoes/Landing";
 import Streaming from "../components/Upload-Search-Stream/Streaming";
 import Upload from "../components/Upload-Search-Stream/Upload";
-
+import Myvidoes from "../components/Home-Myvidoes/Myvideos";
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 function Router(){
     return <>
@@ -11,7 +11,7 @@ function Router(){
                 <Route path="/" element={<LandingPage/>} />
                 <Route path="/streaming" element={<Streaming/>}/>
                 <Route path="/Upload" element={<Upload/>}/>
-               
+                <Route path="/myvidoes" element={<Myvidoes/>}/>
             </Routes>
         </BrowserRouter>
     </>
