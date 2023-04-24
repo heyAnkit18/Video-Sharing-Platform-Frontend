851fdd5f37b94f2574f9800a35dcf5b167967600
Ankit <ankitkumarjune18@gmail.com>
2023-04-24 18:22:45 +0530

    added files

---
+++ video-sharing/src/components/Upload-Search-Streaming/Search.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import axios from "axios";
+import { useState,useEffect} from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "../Home-Myvidoes/Navbar/Navbar";
+import Card from "../Home-Myvidoes/Navbar/Card";
+
+const Search = ()=>{
+    const [videos, setVideos] = useState([]);
+  const query = useLocation().search;
+    console.log(query)
+    useEffect(() => {
+        axios.get(`http://localhost:8080/search${query}`)
+            .then((res) => {
+                setVideos(res.data)
+            }).catch((err) => {
+                console.log(err)
+                window.alert("something went wrong")
+
+            })
+    }, [query]);
+  console.log(videos[0])
+    return <>
+        <Navbar />
+        <div style={{display:"flex",flexWrap:"wrap",gap:"15px",justifyContent:"space-around",marginTop:"20px"}}>
+            {videos.map((data)=>{
+                return <Card data={data}/>
+            })}
+        </div>
+    </>
+}
+export default Search
